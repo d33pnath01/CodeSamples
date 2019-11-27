a68b239a15522d2c358f19c37e1cff964e10be4f
Deeprashmi Nath <deepnath01@gmail.com>
2019-11-26 19:48:25 -0700

    Code Sample for Fade

---
+++ Fade Code Sample/RotateTunnel.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RotateTunnel : MonoBehaviour
+{
+    #region Instance Variables
+    [SerializeField]
+    [Tooltip("The amount to rotate left (angles ) the tunnel")]
+    private float rotationAmountLeft = 90.0f;
+    [SerializeField]
+    [Tooltip("The amount to rotate right (angles negative) the tunnel")]
+    private float rotationAmountRight = -90.0f;
+    [SerializeField]
+    [Tooltip("The amount of time it takes to rotate the tunnel (in seconds)")]
+    private float timeToRotate = 1.0f;
+    [SerializeField]
+    [Tooltip("The ball object itself")]
+    private GameObject ball = null;
+    #endregion
+
+    #region Hidden Variables
+    private Vector3 rotationPoint = Vector3.zero;
+    private bool isRotating = false;
+    private PlayerCollision playerCollision = null;
+    #endregion
+
+    private void Start()
+    {
+        playerCollision = ball.GetComponent<PlayerCollision>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            RotateLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RotateRight();
+        }
+    }
+
+    public void RotateLeft()
+    {
+        if (CanRotate())
+        {
+            isRotating = true;
+            StartCoroutine(RotateTunnelOverTime(timeToRotate, rotationAmountLeft));
+        }
+    }
+
+    public void RotateRight()
+    {
+        if (CanRotate())
+        {
+            isRotating = true;
+            StartCoroutine(RotateTunnelOverTime(timeToRotate, rotationAmountRight));
+        }
+    }
+
+    private bool CanRotate()
+    {
+        if (playerCollision != null && !isRotating && playerCollision.CheckOnGround())
+            return true;
+        return false;
+    }
+
+    private IEnumerator RotateTunnelOverTime(float timeToRotate, float rotateAmount)
+    {
+        if(ball != null)
+        {
+            float currentRotation = this.gameObject.transform.eulerAngles.z;
+            float targetRotation = currentRotation + rotateAmount;
+
+            float elapsedTime = 0;
+            if(ball != null)
+                ball.GetComponent<Rigidbody>().isKinematic = true;
+            while (elapsedTime < timeToRotate)
+            {
+                elapsedTime += Time.deltaTime;
+                float zRotationAmount = Mathf.Lerp(currentRotation, targetRotation, elapsedTime / timeToRotate);
+                this.gameObject.transform.RotateAround(rotationPoint, Vector3.forward, zRotationAmount - this.gameObject.transform.eulerAngles.z);
+
+                yield return null;
+            }
+            isRotating = false;
+            if(ball != null)
+                ball.GetComponent<Rigidbody>().isKinematic = false;
+
+            playerCollision.CheckForPitDeath();
+        }
+     
+    }
+}
---
+++ Fade Code Sample/WallManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    #region MemberVariables
+    // Tunnel Prefabs
+    [SerializeField]
+    private GameObject[] earlyTunnelPrefabs = null;
+    [SerializeField]
+    private GameObject[] midTunnelPrefabs = null;
+    [SerializeField]
+    private GameObject[] lateTunnelPrefabs = null;
+
+    // Location to Generate
+    [SerializeField]
+    private Transform playerTransform = null;
+    private float tunnelSpawnLocationZ = 0.0f;
+
+    // Tunnel Details
+    [SerializeField]
+    private float tunnelLength = 60.0f;
+    //private int tunnelsOnScreen = 2;
+
+    // Keep Track of Active Tunnels
+    private List<GameObject> activeTunnels = null;
+    private int lastTunnelIndex = 0;
+    private int numberOfTunnelsGenerated = 0;
+    #endregion
+
+    #region Start
+    void Start()
+    {
+        // Holds the tunnel objects on the screen
+        activeTunnels = new List<GameObject>();
+        SpawnTunnel(earlyTunnelPrefabs);
+    }
+    #endregion
+
+    #region Update
+    void Update()
+    {
+        // Spawning Tunnels
+        if(playerTransform.position.z > (tunnelSpawnLocationZ - (tunnelLength / 2)))
+        {
+            if(numberOfTunnelsGenerated < 3)
+            {
+                SpawnTunnel(midTunnelPrefabs);
+            }
+            else
+            {
+                SpawnTunnel(lateTunnelPrefabs);
+            }
+        }
+
+        // Deleting Tunnels
+        if(activeTunnels.Count > 2)
+        {
+            DeleteTunnel();
+        }
+    }
+    #endregion
+
+    #region SpawnTunnel
+    private void SpawnTunnel(GameObject[] tunnelPrefabs)
+    {
+        GameObject tunnel;
+        tunnel = Instantiate(tunnelPrefabs[RandomTunnelIndex(tunnelPrefabs)]) as GameObject;
+        tunnel.transform.position = Vector3.forward * tunnelSpawnLocationZ;
+        tunnelSpawnLocationZ += tunnelLength;
+        numberOfTunnelsGenerated++;
+        activeTunnels.Add(tunnel);
+        tunnel.transform.SetParent(this.transform);
+    }
+    #endregion
+
+    #region DeleteTunnel
+    private void DeleteTunnel()
+    {
+        Destroy(activeTunnels[0]);
+        activeTunnels.RemoveAt(0);
+    }
+    #endregion
+
+    #region RandomTunnelIndex
+    // Random Index Generator
+    private int RandomTunnelIndex(GameObject[] tunnelPrefabs)
+    {
+        if(tunnelPrefabs.Length <= 1)
+        {
+            return 0;
+        }
+
+        int randomIndex = lastTunnelIndex;
+        while(randomIndex == lastTunnelIndex)
+        {
+            randomIndex = Random.Range(0, tunnelPrefabs.Length);
+        }
+
+        lastTunnelIndex = randomIndex;
+        return randomIndex;
+    }
+    #endregion
+}
