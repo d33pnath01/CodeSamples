cacf62d4377e83a7bbd3e51e91215ecbe3bc0cd5
Deeprashmi Nath <deepnath01@gmail.com>
2019-11-26 18:27:55 -0700

    Code Sample for Reef Rescue

---
+++ Reef Rescue Code Sample/CoralDiscCollider.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class CoralDiscCollider : MonoBehaviour
+{
+    #region Instance Variables
+    [Tooltip("The parent object")]
+    [SerializeField]
+    private GameObject parentObjectDisc = null;
+    #endregion
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Coral"))
+        {
+            // Positioning
+            Vector3 spawnLocation = new Vector3();
+            spawnLocation.x = gameObject.transform.position.x;
+            spawnLocation.y = gameObject.transform.position.y + 0.02f;
+            spawnLocation.z = gameObject.transform.position.z;
+
+            // Rotation
+            Quaternion spawnRotation = Quaternion.Euler(collision.gameObject.GetComponent<CutCoralPiece>().getOriginalRotation());
+
+            // Creation and Destruction
+            GameObject spawnedObject = Instantiate(collision.gameObject, spawnLocation, spawnRotation);
+
+            spawnedObject.transform.localScale = collision.gameObject.GetComponent<CutCoralPiece>().getOriginalScale();
+            spawnedObject.tag = "Untagged";
+
+            // remove interaction from spawned object and set parent to disc
+            RemoveInteractable(spawnedObject);
+            spawnedObject.transform.parent = parentObjectDisc.transform;
+
+            // remove interactable and the game object from the scene
+            // interable removale is needed to unmap hand before we call delete
+            RemoveInteractable(collision.gameObject);
+            collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+
+            // reset material if needed
+            this.gameObject.GetComponent<RemoveHighlightedMaterial>().removeMaterial(collision.gameObject);
+            this.gameObject.GetComponent<RemoveHighlightedMaterial>().removeMaterial(spawnedObject);
+
+            // ready to be placed in pool
+            parentObjectDisc.tag = "CoralStubs";
+
+            parentObjectDisc.GetComponent<BoxCollider>().enabled = true;
+            parentObjectDisc.GetComponent<Rigidbody>().useGravity = true;
+
+            // destroy the collision point
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void RemoveInteractable(GameObject interactableGameObject)
+    {
+        Destroy(interactableGameObject.GetComponent<BoxCollider>());
+        Destroy(interactableGameObject.GetComponent<SteamVR_Skeleton_Poser>());
+        Destroy(interactableGameObject.GetComponent<InteractableHoverEvents>());
+        Destroy(interactableGameObject.GetComponent<Throwable>());
+        Destroy(interactableGameObject.GetComponent<Interactable>());
+        Destroy(interactableGameObject.GetComponent<Rigidbody>());
+    }
+}
---
+++ Reef Rescue Code Sample/UnderwaterStubs.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class UnderwaterStubs : MonoBehaviour
+{
+    public GameObject playerObject;
+    public AudioSource OGBG = null, Station6BG = null, Station6Intro = null;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Coral"))
+        {
+            // Make Parent
+            collision.gameObject.GetComponent<Interactable>().hideHandOnAttach = false;
+            // remove interactability
+            RemoveInteractable(collision.gameObject);
+
+            // fix material if highlighted material was last one set
+            this.gameObject.GetComponent<RemoveHighlightedMaterial>().removeMaterial(collision.gameObject);
+
+            // set position
+            collision.gameObject.transform.position = this.gameObject.transform.position;
+
+            // remove colliders and rigid body
+            collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            collision.gameObject.SetActive(false);
+            collision.gameObject.GetComponent<BoxCollider>().enabled = false;
+            Destroy(collision.gameObject.GetComponent<Rigidbody>());
+            collision.gameObject.SetActive(true);
+            Destroy(this.gameObject);
+
+            GameManager.s_underwaterCoralStubs++;
+
+            // if true the game is over play final music and audio voice over
+            if (GameManager.s_underwaterCoralStubs == 4)
+            {
+                OGBG.Stop();
+                Station6BG.Play();
+                Station6Intro.Play();
+                // teleport player to final area (bad hardcoding)
+                playerObject.transform.position = new Vector3(-234.91f, 3.345f, 53.299f);
+            }
+        }
+    }
+
+    private void RemoveInteractable(GameObject interactableGameObject)
+    {
+        interactableGameObject.SetActive(false);
+        Destroy(interactableGameObject.GetComponent<BoxCollider>());
+        Destroy(interactableGameObject.GetComponent<SteamVR_Skeleton_Poser>());
+        Destroy(interactableGameObject.GetComponent<InteractableHoverEvents>());
+        Destroy(interactableGameObject.GetComponent<Throwable>());
+        Destroy(interactableGameObject.GetComponent<Interactable>());
+        Destroy(interactableGameObject.GetComponent<Rigidbody>());
+        interactableGameObject.SetActive(true);
+    }
+}
